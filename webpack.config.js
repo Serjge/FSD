f67b11680f33b1a60e6c1ca1df3440c631f47ab9
Serjge <serjge@yandex.ru>
2021-09-07 16:14:31 +0300

    add loders

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,11 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  context: path.resolve(__dirname, 'src'),
   mode: 'development',
   entry: {
-    main: './src/index.js',
-    analitics: './src/analitics.js',
+    main: './index.js',
+    analitics: './analitics.js',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -14,8 +15,24 @@ module.exports = {
   },
   plugins: [
     new HTMLWebpackPlugin({
-      template: './src/index.html',
+      template: './index.html',
     }),
     new CleanWebpackPlugin(),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|svg|giv)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(ttf|woff|woff2|oet)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
 };
