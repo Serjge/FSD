95702bb381a596502c1c26a89282e0f5c390df0e
Serjge <serjge@yandex.ru>
2021-09-12 14:56:14 +0300

    add copy-wepack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
@@ -26,11 +27,22 @@ module.exports = {
       chunks: 'all',
     },
   },
+  devServer: {
+    port: 4200,
+  },
   plugins: [
     new HTMLWebpackPlugin({
       template: './index.html',
     }),
     new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'), // откуда копировать файл или папку
+          to: path.resolve(__dirname, 'dist'), // куда копировать файл или папку
+        },
+      ],
+    }),
   ],
   module: {
     rules: [
